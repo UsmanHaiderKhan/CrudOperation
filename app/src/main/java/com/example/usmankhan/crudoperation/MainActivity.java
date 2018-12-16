4f3cd9f730ce2577ab7db15ca23c9a53c2447ed7
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-12-16 22:06:52 +0500

    Update Declare and Initilize the textview and edit textbox

--- app/src/main/java/com/example/usmankhan/crudoperation/MainActivity.java
+++ app/src/main/java/com/example/usmankhan/crudoperation/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.usmankhan.crudoperation;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,5 +14,17 @@ EditText fullname,degree,year;
         fullname=(EditText)findViewById( R.id.stu_name );
         degree=(EditText)findViewById( R.id.deg_name );
         year=(EditText)findViewById( R.id.std_year );
+    }
+
+    public void add_student(View view) {
+    }
+
+    public void update_student(View view) {
+    }
+
+    public void delete_student(View view) {
+    }
+
+    public void view_sudents(View view) {
     }
 }
